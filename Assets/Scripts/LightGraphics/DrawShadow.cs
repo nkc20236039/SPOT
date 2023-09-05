eb90b7f65b948d83f284e36736e57b0513e7f1ec
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2023-09-05 19:40:45 +0900

    Update: スプライトの仕分けを調整

--- Assets/Scripts/LightGraphics/DrawShadow.cs
+++ Assets/Scripts/LightGraphics/DrawShadow.cs
@@ -6,28 +6,28 @@ public class DrawShadow : MonoBehaviour
 {
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        // メッシュの作成  
+        // メッシュの作成
         var mesh = new Mesh();
 
-        // 頂点座標配列をメッシュにセット  
+        // 頂点座標配列をメッシュにセット
         mesh.SetVertices(new Vector3[] {
             new Vector3 (-8.889f, -5),
-            new Vector3 (8.889f, -5f),
-            new Vector3 (8.889f, 5f),
             new Vector3 (-8.889f, 5f),
+            new Vector3 (8.889f, 5f),
+            new Vector3 (8.889f, -5f),
         });
 
-        // インデックス配列をメッシュにセット  
+        // インデックス配列をメッシュにセット
         mesh.SetTriangles(new int[] {
-            0, 1, 2, 0, 3, 2
+            0, 1, 2, 0, 2, 3
         }, 0);
 
-        // MeshFilterを通してメッシュをMeshRendererにセット  
+        // MeshFilterを通してメッシュをMeshRendererにセット
         MeshFilter filter = GetComponent<MeshFilter>();
         filter.sharedMesh = mesh;
     }

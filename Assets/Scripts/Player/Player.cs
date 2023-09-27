8d6aa0441fa6fca701295d305a1c10fa08269741
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2023-09-27 19:05:52 +0900

    Update: スポットライトがオブジェクトに埋まることを防止

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,7 @@ public partial class Player : MonoBehaviour
 {
     public int lightDirection { get; private set; } = 1;
     public Vector2 lightCallPosition;
+    public bool haveLight = true;                 // ライトの所持状態
 
     private Vector2 moveInput;                // 移動方向取得
 
@@ -13,23 +14,26 @@ public partial class Player : MonoBehaviour
     private Vector2 velocity;
     private Animator animator;
     private bool isRightClicking;
-    private bool haveLight = true;                 // ライトの所持状態
     private bool onGravity;                 // 重力をつける
     private bool isJump;                    // ジャンプ
     private bool isFall;
     private float mouseDelta;               // マウスの移動量
     private Coroutine jumpCoroutine;
+    private SpriteRenderer spotLightSpriteRenderer; // ライトの設置/持っているときのスプライト
+    private bool chengedDirection = false;
 
-    [SerializeField] float detectionRange;
-    [SerializeField] Vector2 distanceToLight;
-    [SerializeField] GameObject spotLight;
-    [SerializeField] LayerMask stageLayer;
+    [SerializeField] private Sprite[] spotLightSprite;
+    [SerializeField] private float detectionRange;
+    [SerializeField] private Vector2 distanceToLight;
+    [SerializeField] private GameObject spotLight;
+    [SerializeField] private LayerMask stageLayer;
 
     void Start()
     {
         groundStateScript = GetComponent<GroundState>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spotLightSpriteRenderer = spotLight.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -49,10 +53,6 @@ public partial class Player : MonoBehaviour
             {
                 PlayAnimation(animationType.Run);
             }
-            else
-            {
-                PlayAnimation(animationType.Idle);
-            }
         }
         else
         {
@@ -63,24 +63,38 @@ public partial class Player : MonoBehaviour
         if (haveLight)
         {
             ChangeSpotLightDirection();
+            mouseDelta = Input.GetAxis("Mouse X");
         }
 
         // スポットライトの方向を調整
-        mouseDelta = Input.GetAxis("Mouse X");
-
-        if (Mathf.Abs(mouseDelta) >= detectionRange && Input.GetMouseButton(1))
+        if (Mathf.Abs(mouseDelta) >= detectionRange && Input.GetMouseButton(1) && !chengedDirection)
         {
             // マウスを動かした方へライトを向けれるようにする
             lightDirection = (int)Mathf.Sign(mouseDelta);
+            chengedDirection = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            // ボタンを離したら再度使えるようにする
+            chengedDirection = false;
         }
 
         // ライトの持ち替え
         if (Input.GetKeyDown(KeyCode.F))
         {
             haveLight = !haveLight;
+
+            // 切り替えた後のスプライトの状態
+            if (haveLight)
+            {
+                spotLightSpriteRenderer.sprite = spotLightSprite[1];
+            }
+            else
+            {
+                spotLightSpriteRenderer.sprite = spotLightSprite[0];
+            }
         }
-
-
     }
 
     private void FixedUpdate()

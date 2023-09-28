bc8b42f39258438ba6318a217dd2723be52f2cde
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2023-09-28 16:09:32 +0900

    Update: UIの追加
    リロードボタンを追加

---
+++ Assets/Scripts/UI/Reload.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Reload : MonoBehaviour
+{
+
+
+    public void ClickEvent()
+    {
+
+    }
+
+    public void MouseEnter()
+    {
+        // マウスがフォーカスされたとき拡大する
+        transform.DOScale(1.25f, 0.5f);
+    }
+
+    public void MouseExit()
+    {
+        // マウスがフォーカスされたとき拡大する
+        transform.DOScale(1f, 0.5f);
+    }
+}
+
